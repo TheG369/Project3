333c4787c1847c3b762ebc6cbde86375e1ee5f5b
Giovani <rang990603@gmail.com>
2024-05-31 00:34:20 -0600

    adding new funtion

--- script.js
+++ script.js
@@ -4,8 +4,21 @@ function mostrarSabores() {
     let sabor = document.querySelector("#nameSabor")
     sabor.innerHTML = sabores[Math.floor(Math.random() * sabores.length) ]
 
-    console.log("Es tu sabor")
 
+    const greetingday = ["Holi Crayoli", "Hi", "Hola que tal"]
+    let greetingDis = document.querySelector("#greeting")
+    greetingDis.innerHTML = greetingday[Math.floor(Math.random() * greetingday.length) ]
+
+    giveEmoji()
+
+    console.log("funcion")
+
+}
+
+function giveEmoji (){
+    const emoji = ["😎", "👽", "👌"]
+    let greetingDis = document.querySelector("#emojis")
+    greetingDis.innerHTML = emoji[Math.floor(Math.random() * emoji.length)]
 }
 
 mostrarSabores()
